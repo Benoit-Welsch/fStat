f86de7060abf6e28f5b7dbb60a298a0dda629a0c
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-10-15 11:07:18 +0000

    Feat : pourcentage on tableconsole

--- src/lib/console.ts
+++ src/lib/console.ts
@@ -1,12 +1,27 @@
-export function generateTable(data: Record<string, any[]>): string {
-  const keys = Object.keys(data).sort((a, b) => data[a].length > data[b].length ? -1 : 1);
+type options = {
+  total?: boolean;
+  pourcentage?: boolean;
+};
+
+export function generateTable(data: Record<string, any[]>, options?: options): string {
+  const keys = Object.keys(data).sort((a, b) => data[a].length > data[b].length ? -1 : 1)
+
+  const totalData = keys.reduce((acc, key) => acc + data[key].length, 0);
+
+  const makeHeader = (key: string) => {
+    if (options && options.pourcentage) {
+      return `${key} (${data[key].length} - ${((data[key].length / totalData) * 100).toFixed(2)}%)`;
+    } else {
+      return `${key} (${data[key].length})`;
+    }
+  }
 
   const maxLengths = keys.map(key => {
-    return Math.max(`${key} (${data[key].length})`.length, ...data[key].map(value => value !== undefined ? String(value).length : 0));
+    return Math.max(makeHeader(key).length, ...data[key].map(value => value !== undefined ? String(value).length : 0));
   });
 
   const header = keys.map((key, i) => {
-    return `${key} (${data[key].length})`.padEnd(maxLengths[i]);
+    return makeHeader(key).padEnd(maxLengths[i]);
   }).join(' | ');
 
   const separator = keys.map((_, i) => '-'.repeat(maxLengths[i])).join('-+-');
@@ -19,7 +34,7 @@ export function generateTable(data: Record<string, any[]>): string {
       return String(value).padEnd(maxLengths[j]);
     }
   }).join(' | '));
-  
+
   return `${header.toUpperCase()}\n${separator}\n${rows.join('\n')}`;
 }
 
